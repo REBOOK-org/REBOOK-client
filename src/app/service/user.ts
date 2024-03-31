d87121d52e956e118e155beed103cef77a20c1a1
Sarah hassan <sarraree5@gmail.com>
2024-03-31 08:41:14 +0200

    feat: create api service using RTK query

---
+++ src/app/service/user.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+const baseUrl = import.meta.env.VITE_REACT_APP_BASE_URL
+console.log(baseUrl)
+
+export const userApi = createApi({
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
+  endpoints: (builder) => ({
+    addUser: builder.mutation({
+      query: (user) => ({
+        url: 'user/signup',
+        method: 'POST',
+        body: user,
+      }),
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useAddUserMutation } = userApi
