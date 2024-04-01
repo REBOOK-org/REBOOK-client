6636778179993d56070a437bd6194154ce1d59ef
Sarah hassan <sarraree5@gmail.com>
2024-04-01 13:25:09 +0200

    ui: logo redirect to home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 
 import { useState } from 'react'
 import LoginSignupModal from './LoginSignupModal'
+import { useNavigate } from 'react-router-dom'
 
 export default function Header() {
+  const navigate = useNavigate()
+
   const [modalOpen, setModalOpen] = useState<'login' | 'signup' | null>(null)
   function setOpenModal(value: 'login' | 'signup' | null) {
     setModalOpen(value)
@@ -19,10 +22,12 @@ export default function Header() {
     <header className="py-4 border-b fixed top-0 left-0 w-full bg-white shadow-md z-50 ">
       <LoginSignupModal modalOpen={modalOpen} closeModal={closeModal} />
       <div className=" mx-3 flex justify-between items-center ">
-        <div className="flex justify-center items-center text-[#9C6644] font-bold lg:text-3xl md:text-2xl text-xl">
-          <span className="text-3xl  font-mono">re</span>
-          <LibraryBig className="text-red-400" width={40} height={35} />
-        </div>
+        <Link to={'/'}>
+          <div className="flex justify-center items-center text-[#9C6644] font-bold lg:text-3xl md:text-2xl text-xl">
+            <span className="text-3xl  font-mono">re</span>
+            <LibraryBig className="text-red-400" width={40} height={35} />
+          </div>
+        </Link>
         <div className="flex items-center">
           <div className=" content-center font-bold text-center  mr-5 ">
             <Link to="/book-upload">Upload your book</Link>
